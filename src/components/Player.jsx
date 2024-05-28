b92017be2cc14355e169100af1b011f882bd1675
Leonid <Leonideko1@gmail.com>
2024-05-28 21:28:34 +0300

    add boardgame component and input for name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,19 +1,24 @@
 import React, { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName)
   const [isEditing, setIsEditing] = useState(false);
 
   const handleEdit = () => {
-    setIsEditing(!isEditing);
+    setIsEditing((editing) => !editing);
   };
+
+  function handleChange(e){
+    setPlayerName(e.target.value)
+  }
 
   return (
     <li>
       <span className="player">
         {isEditing ? (
-          <input type="text" required value={name}/>
+          <input type="text" required value={playerName} onChange={handleChange}/>
         ) : (
-          <span className="player-name">{name}</span>
+          <span className="player-name">{playerName}</span>
         )}
 
         <span className="player-symbol">{symbol}</span>
